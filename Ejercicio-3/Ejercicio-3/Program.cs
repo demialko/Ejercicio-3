1e424f8c799e4699e265180384db07887bf09995
demialko <demialko@gmail.com>
2021-03-24 11:32:07 -0300

    Solucion ejercicio 3

---
+++ Ejercicio-3/Ejercicio-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tecla = "";
+            Console.WriteLine("Para finalizar el proceso se debe presionar la tecla X");
+            do
+            {
+                tecla = Console.ReadLine().ToUpper();
+                if (tecla != "X")
+                {
+                    Console.WriteLine("Se debe ingresar la tecla X para finalizar el proceso, usted ha ingresado la tecla " + tecla);
+                }
+            } while (tecla != "X");
+            Console.WriteLine("El proceso ha finalizado con exito");
+
+            Console.ReadKey();
+        }
+    }
+}
